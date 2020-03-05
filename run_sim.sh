5d740a0062d61882f6a2f2720e38ce7ce59936cc
Edwin Peguero <emp238@cornell.edu>
2020-03-05 15:34:46 -0500

    fixed bug in 'run_sim.sh'

--- run_sim.sh
+++ run_sim.sh
@@ -11,7 +11,7 @@ ssh emp238@gorgonzola.cs.cornell.edu \
    timeout 20 ./build/RVSP/gem5.opt -d lotus/vvadd/result \
    \configs/phil/brg_hammerblade.py \
    --cmd=lotus/vvadd/vvadd\
-   --num-cpu=4 \ &&'
+   --num-cpu=4 \'
 
 # append something like this to also output stats results
 # cat lotus/vvadd/results/stats.txt | grep -E "(numCycles|CPI)"'
