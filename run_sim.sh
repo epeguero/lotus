6383712b0929b8d404cd6b3ac6c38c39e20d5a77
Edwin Peguero <emp238@cornell.edu>
2020-03-04 22:32:15 -0500

    modified script 'run_sim.sh' to completely remove project files remotely, to ensure a clean run when refactoring runtime libraries.

--- run_sim.sh
+++ run_sim.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
 # copy kernel to gorgonzola
+ssh emp238@gorgonzola.cs.cornell.edu "rm -rf ~/gem5-mesh/lotus/*"
+
 scp -r example/* emp238@gorgonzola.cs.cornell.edu:~/gem5-mesh/lotus/
 
 ssh emp238@gorgonzola.cs.cornell.edu \
   'cd ~/gem5-mesh &&\
-   make clean -C lotus/vvadd && 
    make -C lotus/vvadd && 
    timeout 20 ./build/RVSP/gem5.opt -d lotus/vvadd/result \
    \configs/phil/brg_hammerblade.py \
    --cmd=lotus/vvadd/vvadd\
-   --num-cpu=4 \ &&
-   cat lotus/vvadd/results/stats.txt | grep -E "(numCycles|CPI)"'
+   --num-cpu=4 \ &&'
+
+# append something like this to also output stats results
+# cat lotus/vvadd/results/stats.txt | grep -E "(numCycles|CPI)"'
